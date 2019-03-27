91ea11707e08d9112bb22b8ff001c0da8daa1589
delcadead <francisdelca@hotmail.com>
2019-03-27 12:55:22 -0500

    Add files via upload

--- tusEntradas.php
+++ tusEntradas.php
@@ -1,24 +1,26 @@
 <?php
-require_once('conexion.php');
+require('conexion.php');
 $rel = $conexion -> query("SELECT * FROM eventos"); 
 $titulo=array();
 while($row = $rel -> fetch_array())
 {
     $titulo[] = $row['titulo'];
 }
-
-require_once('userConnect.php');
 session_start();
 $Usuario=$_SESSION['usuarivalido']['usuario'];
 $limit = count($titulo);
 for($i = 0; $i < $limit; $i++)
 {
+    require('userConnect.php');
     $t = str_replace(" ", "_", $titulo[$i]);
     $row = $conexion -> query("SELECT * FROM $t")->fetch_array();
-
-
-    
-    echo $row['usuario']."<br>";
+    if($row['usuario'] == $Usuario)
+    {
+        $id = $row['idevento'];
+        require('conexion.php');
+        $title = $conexion -> query("SELECT * FROM eventos WHERE id = $id")->fetch_array(); 
+        echo $title['titulo']."<br>";
+    }
 }
 
 ?>
